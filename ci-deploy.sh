807daf0292d8288ea17bbc758fbebb94bb570e5d
yihanwu1024 <48046869+yihanwu1024@users.noreply.github.com>
2021-10-16 14:15:05 -0500

    Update ci-deploy.sh

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -1,11 +1,10 @@
 cd ..
 git clone "https://${CI_TOKEN}@github.com/IT50/it50.github.io"
+rm it50.github.io/*
 book/build.sh it50.github.io
 cd it50.github.io
 git config user.name CI
 git config user.email ci@it50.org
 git add .
-echo "Before commit"
 git commit -m "Update site"
-echo "After commit"
 git push
