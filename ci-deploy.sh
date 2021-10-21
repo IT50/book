39cb5efdf7c373cc3961289740095153d6fbdd4b
yihanwu1024 <48046869+yihanwu1024@users.noreply.github.com>
2021-10-20 21:42:42 -0500

    Update ci-deploy.sh

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -3,7 +3,8 @@ git clone "https://${CI_TOKEN}@github.com/IT50/it50.github.io"
 cd it50.github.io
 shopt -s extglob
 rm -r -v !("CNAME"|"README")
-../book/build.sh .
+cd ..
+book/build.sh it50.github.io
 git config user.name CI
 git config user.email ci@it50.org
 git add .
